7d443aec9f83d945904894da58397a0d11b5d350
baldwin <baldwinmalabanan.bm@gmail.com>
2026-03-06 00:28:28 -0500

    shared_data/server_critical_data.js: Replace local psql to cloud psql [neon]

--- shared_data/server_critical_data.js
+++ shared_data/server_critical_data.js
@@ -4,14 +4,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 import SERVER_PORT from './common_critical_data.js';
+
 const SERVER_CRITICAL_DATA = {
-  SERVER_PORT: SERVER_PORT,
+  SERVER_PORT,
   BUILD_PATH: `${__dirname}/build/`,
-  DATABASE_HOST: 'localhost',
-  DATABASE_USER: 'postgres',
-  DATABASE_PW: 'b_malab',
-  CODE_NOTES_DATABASE: 'code_notes_db',
-  SPLICE_LOG_DATABASE: 'postgres',
+  // Neon postgres connection string:
+  // postgres://<user>:<password>@<host>/<db>?sslmode=require
+  NEON_DATABASE_URL: process.env.NEON_DATABASE_URL || '',
   DEBUG_STATE: false
 };
 
